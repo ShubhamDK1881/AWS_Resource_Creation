780db2d5cb535907c6453fefcb8e0e8aca11edac
ShubhamDK1881 <163509278+ShubhamDK1881@users.noreply.github.com>
2024-05-30 00:41:33 +0530

    Test Demo New 8

--- Resources/lambda-function/index.js
+++ Resources/lambda-function/index.js
@@ -2,6 +2,6 @@ exports.handler = async (event) => {
     console.log('Event:', event);
     return {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+        body: JSON.stringify('Hello from Lambda by Shubham!'),
     };
 };
